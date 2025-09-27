a4d09e2057aae75c8fffe6648bc099e3229e2d8e
cyber3x <neven.lukic8@gmail.com>
2025-09-28 01:06:56 +0200

    minor

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -8,7 +8,7 @@ export async function GET(context) {
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => ({
+    items: posts.filter(post => post.data.public).map((post) => ({
       ...post.data,
       link: `/posts/${post.slug}/`,
     })),
